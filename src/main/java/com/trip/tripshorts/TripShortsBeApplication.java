50da4d9bcb1cc09ae395bd9d63133ecb22e831b7
leedongkyu0407 <use02012@gmail.com>
2024-11-11 20:34:21 +0900

    chore: 프로젝트 백엔드 초기 설정 완료
    - Spring Boot 프로젝트 생성
    - 필요한 의존성 추가(JPA, MyBatis, Spring Security 등)
    - application.yml.example 추가
    - .gitignore 설정
    - .gitattributes에 줄바꿈 관련 설정 추가

---
+++ src/main/java/com/trip/tripshorts/TripShortsBeApplication.java
@@ -0,0 +1,13 @@
+package com.trip.tripshorts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripShortsBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripShortsBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trip/tripshorts/TripShortsBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trip.tripshorts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripShortsBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
