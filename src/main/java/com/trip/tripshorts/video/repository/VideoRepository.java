d21f45dd886ae1ea47b95958d871949d5882d484
wonjoon <seowj819@gmail.com>
2024-11-19 10:27:20 +0900

    fix : 브랜치 병합

--- src/main/java/com/trip/tripshorts/video/repository/VideoRepository.java
+++ src/main/java/com/trip/tripshorts/video/repository/VideoRepository.java
@@ -3,5 +3,8 @@ package com.trip.tripshorts.video.repository;
 import com.trip.tripshorts.video.domain.Video;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface VideoRepository extends JpaRepository<Video, Long> {
+    List<Video> findAllByMemberId(Long memberId);
 }
