b612588241af089a83bedf05f96acf703c9724a6
WonJoon <seowj@gmail.com>
2025-03-09 16:10:05 +0900

    fix: s3ê² 경로 수정

--- src/main/java/com/trip/tripshorts/video/service/HlsConverter.java
+++ src/main/java/com/trip/tripshorts/video/service/HlsConverter.java
@@ -3,12 +3,14 @@ package com.trip.tripshorts.video.service;
 import com.trip.tripshorts.util.S3Service;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.*;
 import java.util.Arrays;
 import java.util.List;
+import java.util.UUID;
 
 @RequiredArgsConstructor
 @Component
@@ -16,6 +18,9 @@ import java.util.List;
 public class HlsConverter {
 
     private final S3Service s3Service;
+
+    @Value("${spring.path.s3}")
+    private String s3Path;
 
     public String convertToHls(MultipartFile videoFile, String outputDir) throws IOException, InterruptedException {
         File outputFolder = new File(outputDir);
@@ -27,11 +32,15 @@ public class HlsConverter {
         File tempFile = File.createTempFile("upload_", ".mp4");
         videoFile.transferTo(tempFile);
 
+        // 변환될 HLS 파일의 UUID 생성
+        String fileName = UUID.randomUUID().toString();
+        String m3u8FilePath = outputDir + "/" + fileName + ".m3u8";
+
         // Fmpeg 명령어 (pipe:0 대신 파일 경로 사용)
         List<String> command = Arrays.asList(
                 "ffmpeg", "-y", "-i", tempFile.getAbsolutePath(), "-profile:v", "baseline", "-level", "3.0",
                 "-s", "640x360", "-start_number", "0", "-hls_time", "5", "-hls_list_size", "0",
-                "-f", "hls", outputDir + "/index.m3u8"
+                "-f", "hls", m3u8FilePath
         );
 
         // FFmpeg 프로세스 실행
@@ -56,11 +65,11 @@ public class HlsConverter {
         tempFile.delete();
 
         // S3 업로드
-        s3Service.uploadHlsFiles(outputDir, "videos/shorts/");
+        s3Service.uploadHlsFiles(outputDir, "videos/hls/");
 
         log.info("S3 업로드 완료");
 
         // S3에 저장된 .m3u8 파일의 URL 반환
-        return "https://tripshorts.s3.ap-northeast-2.amazonaws.com/videos/shorts/index.m3u8";
+        return s3Path + "/videos/hls/" + fileName + ".m3u8";
     }
 }
